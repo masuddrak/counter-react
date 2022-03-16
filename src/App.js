06eb4cd7bc3be7211117f27edc353f785d082a3f
masud rana <masudranaacb@gmail.com>
2022-03-16 17:43:27 +0600

    increase descrease

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -9,9 +10,16 @@ function App() {
   );
 }
 function Counter() {
+  const [count,setCount]=useState(0)
+
+  const IncreaseCount=()=> setCount(count+1);
+  const DecriaseCount=()=> setCount(count-1);
+  
   return (
     <div className="counter">
-      <h1>Counter:</h1>
+      <h1>Counter:{count}</h1>
+      <button onClick={IncreaseCount}>Increase</button>
+      <button onClick={DecriaseCount}>Decrease</button>
     </div>
   );
 }
